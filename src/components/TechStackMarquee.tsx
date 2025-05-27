ea1417adb78af0a195ddd2bfea307af9d682717a
GouravMalviya445 <gouravmalviya445@gmail.com>
2025-05-27 22:39:17 +0530

    update tech stack marqueee component

--- src/components/TechStackMarquee.tsx
+++ src/components/TechStackMarquee.tsx
@@ -1,35 +1,20 @@
 "use client";
 import React from 'react'
-import { RiJavascriptFill } from "react-icons/ri";
-import { BiLogoTypescript } from "react-icons/bi";
-import { SiMongodb } from "react-icons/si";
-import { BiLogoPostgresql } from "react-icons/bi";
-import { FaReact } from "react-icons/fa";
-import { RiNextjsFill } from "react-icons/ri";
-import { RiTailwindCssFill } from "react-icons/ri";
-import { BsGithub } from "react-icons/bs";
 import { motion } from 'motion/react';
+import { techIcons } from './icons';
+import { cn } from '@/lib/utils';
 
-const TechStackSlider = () => {
-  const techIcons = [
-    <FaReact className='backdrop:blur-2xl' />,
-    <RiNextjsFill className='backdrop:blur-2xl'/>,
-    <BiLogoTypescript className='backdrop:blur-2xl'/>,
-    <SiMongodb className='backdrop:blur-2xl'/>,
-    <RiJavascriptFill className='backdrop:blur-2xl'/>,
-    <BiLogoPostgresql className='backdrop:blur-2xl'/>,
-    <RiTailwindCssFill className='backdrop:blur-2xl'/>,
-    <BsGithub className='backdrop:blur-2xl'/>
-  ]
+const TechStackMarquee = () => {
+
   
   return (
-    <div className='overflow-hidden border-y border-accent/10 absolute left-0'>
+    <div className='overflow-x-hidden max-w-screen border-y mt-10 border-accent/10 absolute left-0'>
       <motion.div
         initial={{
-          x: "0"
+          translateX: "0%"
         }}
         animate={{
-          x: "-50%"
+          translateX: "-50%"
         }}
         transition={{
           duration: 15,
@@ -39,13 +24,16 @@ const TechStackSlider = () => {
         }}
         className='h-30 flex items-center gap-20'>
       {[...techIcons, ...techIcons, ...techIcons].map((icon, index) => (
-        <div className='text-4xl text-text-primary/90 rounded-full p-1 dark:bg-green-400/40 shadow-[0_0_20px_#00C16E]  dark:shadow-400/30' key={index}>
-          {icon}
-        </div>
+        <motion.div whileHover={{ scale: 1.1 }} className={cn(
+          'text-4xl rounded-full',
+          "dark:bg-radial from-accent/30 to-accent/20 backdrop-blur-3xl text-accent shadow-accent/30 shadow-[0_0_15px]"
+        )} key={index} >
+          {icon} {/* icon is reactIcons component */}
+        </motion.div>
       ))}
       </motion.div>
     </div>
   )
 }
 
-export default TechStackSlider
+export default TechStackMarquee
