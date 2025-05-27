adec574d4c2bcb98bb415d25076b1ba4d9ccaa56
GouravMalviya445 <gouravmalviya445@gmail.com>
2025-05-27 22:40:02 +0530

    update container component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -6,7 +6,7 @@ const Container = ({
   className = ""
 }: {children: React.ReactNode, className?: string}) => {
   return (
-    <div className={cn("mx-auto max-w-7xl px-4 sm:px-6 lg:px-8", className)}>{children}</div>
+    <div className={cn("mx-auto w-[1200px] px-4 sm:px-6 lg:px-8", className)}>{children}</div>
   )
 }
 
